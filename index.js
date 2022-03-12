7fe6accd80e6be3d2204acba2afbc5d1d1ea617b
kyra qiu <kyraqiu@gmail.com>
2022-03-12 16:19:22 -0500

    commit tree d4ede46bff595354a3d8c36c58e56a77fc012ac2

---
+++ index.js
@@ -0,0 +1,6 @@
+function receivesAFunction(callback) {return callback()};
+
+function returnsANamedFunction() {return function named(){};}
+
+function returnsAnAnonymousFunction () {return function(){};}
+
